b28af0da8b25e907189c509393bed755191721a3
sylvocode <sylvesterokonkwo550@gmail.com>
2026-03-05 16:19:06 +0100

    Task 1: Stop infinite loop, remove unused variable

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - stops an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+    printf("Infinite loop incoming :(\n");
+
+    /* Commented out the infinite loop */
+    /* int i; */
+    /* i = 0; */
+    /* while (i < 10) */
+    /* { */
+    /*     putchar(i); */
+    /* } */
+
+    printf("Infinite loop avoided! \\o/\n");
+
+    return (0);
+}
