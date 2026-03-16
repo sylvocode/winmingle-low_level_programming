3fa52d997fd385f4c766e6979845e9174d63be47
sylvocode <sylvesterokonkwo550@gmail.com>
2026-03-16 18:32:15 +0100

    Task 4: function that prints a string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to print
+ */
+
+void print_rev(char *s)
+{
+ int len = 0;
+
+ while (s[len] != '\0')
+ {
+   len++;
+ }
+
+ len--;
+
+ while (len >= 0)
+ {
+   _putchar(s[len]);
+   len--;
+ }
+
+ _putchar('\n');
+}
