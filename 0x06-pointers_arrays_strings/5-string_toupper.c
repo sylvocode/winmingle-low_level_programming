ea16555281bb80cbc42a14ba8d26e5f947b5a307
sylvocode <sylvesterokonkwo550@gmail.com>
2026-05-11 14:37:22 +0100

    Task 5: convert string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @str: string to chnage
+ * Return: pointer to string
+ */
+
+char *string_toupper(char *str)
+{
+  int i = 0;
+
+  while (str[i] != '\0')
+  {
+    if (str[i] >= 'a' && str[i] <= 'z')
+    {
+      str[i] = str[i] - 32;
+    }
+
+    i++;
+  }
+
+  return (str);
+}
