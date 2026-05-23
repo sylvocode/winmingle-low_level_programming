61e5e4477a16ee2833bdafb14b40a526f03f9b70
sylvocode <sylvesterokonkwo550@gmail.com>
2026-05-23 18:55:38 +0100

    Task 8: set value of pointer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of pointer to char
+ * @s: pointer to pointer
+ * @to: pointer value to set
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
