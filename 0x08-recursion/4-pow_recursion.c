da46c2b9758970bc8e05c20d6d9be08b5f9bf7cd
sylvocode <sylvesterokonkwo550@gmail.com>
2026-05-28 14:08:04 +0100

    Task 4: compute power using recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns x raised to power y
+ * @x: base number
+ * @y: power
+ *
+ * Return: result of x^y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return  (-0);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x *_pow_recursion(x, y - 1));
+}
